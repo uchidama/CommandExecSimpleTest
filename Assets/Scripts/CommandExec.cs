6bf6c0c94bc265f8faa6f27c927a0a91f6bc5410
Uchida Masatoshi <shinishimaru@gmail.com>
2020-10-19 17:24:22 +0900

    first commit

---
+++ Assets/Scripts/CommandExec.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class CommandExec : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var arguments = "test.txt";
+        var exProcess = new Process();
+        exProcess.StartInfo.CreateNoWindow = true;
+        exProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        exProcess.StartInfo.FileName = "/usr/bin/touch";
+        exProcess.StartInfo.Arguments = arguments;
+        exProcess.EnableRaisingEvents = true;
+
+        //外部のプロセスを実行する
+        exProcess.Start();
+        exProcess.WaitForExit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
